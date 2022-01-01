168257ac20285e2f2084e6695035a6c03773c572
durgeshmeena <durgeshkumar@iitg.ac.in>
2022-01-01 11:08:33 +0530

    updating signup validation

--- app/controlars.py
+++ app/controlars.py
@@ -1,11 +1,9 @@
-from app import app
 from flask import flash, session, redirect, url_for
 from functools import wraps
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField
-# from email_validator import validate_email, EmailNotValidError
 from wtforms.validators import Email
-
+import re
 
 
 class LoginForm(FlaskForm):
@@ -30,12 +28,21 @@ class LoginForm(FlaskForm):
 class SignupForm(FlaskForm):
     name = StringField('Name')
     email = StringField('Email', [Email("Email is not valid!!")])
+    username = StringField('Username') 
     password = PasswordField("Password")
 
     def validate(self, extra_validators=None):
         if super().validate(extra_validators):
             
-            if not self.password.data:
+            if not self.username.data:
+                self.username.errors.append('Username is required.')
+                return False
+            elif not bool(re.match("^[a-z][a-z0-9]*(?:-[a-z0-9]+)*$",self.username.data)):
+                self.username.errors.append('Username is not valid.')
+                return False
+
+
+            elif not self.password.data:
                 self.password.errors.append('Password is required.')
                 return False
             elif len(self.password.data)<6:
