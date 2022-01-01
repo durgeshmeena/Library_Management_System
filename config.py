f3e8ec03d63c3d94abf83bad226194b6fe77e613
durgeshmeena <durgeshkumar@iitg.ac.in>
2022-01-01 11:28:26 +0530

    correcting MongoURI

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from os import environ
 from dotenv import load_dotenv, find_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+import certifi
 #---------------------------------
 # Default Config
 #---------------------------------
@@ -17,7 +17,7 @@ load_dotenv(find_dotenv())
 SECRETE_KEY = environ.get('SECRETE_KEY')
 
 # MongoDB setting
-MONGODB_SETTINGS = {'DB':'Lib', 'host':environ.get('MONGO_URI')}
+MONGODB_SETTINGS = {'DB':'LibM', 'host':environ.get('MONGO_URI'),'tlsCAFile':certifi.where()}
 
 # Flask-WTF flag for CSRF
 CSRF_ENABLED = True
